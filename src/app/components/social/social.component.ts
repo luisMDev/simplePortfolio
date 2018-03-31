d2a7997c9e2dd852db515932f9454a6d87f38ac2
luisMDev <lm.torresrivera@gmail.com>
2018-03-31 16:52:53 +0200

    social bar

--- src/app/components/social/social.component.ts
+++ src/app/components/social/social.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social.component.scss']
 })
 export class SocialComponent implements OnInit {
+  public socialList = [
+    {
+      icon: 'fab fa-dribbble',
+      url: 'https://www.uplabs.com/lm_torresrivera'
+    },
+    {
+      icon: 'fab fa-github',
+      url: 'https://github.com/luisMDev'
+    },
+    {
+      icon: 'fab fa-twitter',
+      url: 'https://twitter.com/LuisMDev'
+    },
+    {
+      icon: 'fab fa-facebook',
+      url: 'https://www.facebook.com/Luis-M-Torres-2064611147149338'
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
